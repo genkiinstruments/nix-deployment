cc68533ebe3c9118ceb59f3ffc4c12cdbe5d5508
Olafur Bogason <olafur@genkiinstruments.com>
2023-12-14 23:13:22 +0000

    & is invalid..

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,7 @@
     description = "Turn on LED on GPIO pin 18";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.libgpiod}/bin/gpioset -c gpiochip0 18=1 &";
+      ExecStart = "${pkgs.libgpiod}/bin/gpioset -c gpiochip0 18=1";
       Type = "oneshot";
       RemainAfterExit = true;
     };
