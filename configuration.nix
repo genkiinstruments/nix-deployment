d8be6d797035f82d5f81882aac47360d4fba7f0c
Olafur Bogason <olafur@genkiinstruments.com>
2023-12-14 23:10:35 +0000

    Another version of the LED code as the previous one turns off after approx 3 seconds..

--- configuration.nix
+++ configuration.nix
@@ -9,22 +9,57 @@
 #    ./examples/xfce4.nix
 #    ./examples/netdata.nix
 #    ./examples/vaultwarden.nix
-    ./examples/led.nix
+    #./examples/led.nix
   ];
   environment.systemPackages = with pkgs; [
     vim
     git
     libgpiod
     qrencode
+    procps
   ];
   services.openssh.enable = true;
   users = {
     users.default = {
       password = "default";
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "gpio" ];
+    };
+    groups.gpio = {};
+  };
+
+#   systemd.user.services.blinkLED = {
+#    description = "Blink LED after rebuild";
+#    after = [ "default.target" ];
+#    wantedBy = [ "default.target" ];
+#    serviceConfig = {
+#      ExecStart = let
+#        gpioset="${pkgs.libgpiod}/bin/gpioset";
+#      in ''
+#        ${gpioset} gpioset -c gpiochip0 18=1 &
+#      '';
+#      User = "default";
+#      Group = "default";
+#    };
+#  };
+
+  systemd.services.turnOnLED = {
+    description = "Turn on LED on GPIO pin 18";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.libgpiod}/bin/gpioset -c gpiochip0 18=1 &";
+      Type = "oneshot";
+      RemainAfterExit = true;
     };
   };
+
+  # Change permissions gpio devices
+  services.udev.extraRules = ''
+    SUBSYSTEM=="bcm2835-gpiomem", KERNEL=="gpiomem", GROUP="gpio",MODE="0660"
+    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio  /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+    SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add",RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+  '';
+
   system.autoUpgrade = {
     allowReboot = true;
     enable = true;
