58942dbc3ccef2afeffbb22ab4bf87f5867ddcfe
Olafur Bogason <olafur@genkiinstruments.com>
2023-12-14 23:45:08 +0000

    Turn the LED back on

--- configuration.nix
+++ configuration.nix
@@ -43,15 +43,15 @@
 #    };
 #  };
 
-# systemd.services.turnOnLED = {
-#   description = "Turn on LED on GPIO pin 18";
-#   wantedBy = [ "multi-user.target" ];
-#   serviceConfig = {
-#     ExecStart = "${pkgs.libgpiod}/bin/gpioset -c gpiochip0 18=1";
-#     Type = "oneshot";
-#     RemainAfterExit = true;
-#   };
-# };
+ systemd.services.turnOnLED = {
+   description = "Turn on LED on GPIO pin 18";
+   wantedBy = [ "multi-user.target" ];
+   serviceConfig = {
+     ExecStart = "${pkgs.libgpiod}/bin/gpioset -c gpiochip0 18=1";
+     Type = "oneshot";
+     RemainAfterExit = true;
+   };
+ };
 
   # Change permissions gpio devices
   services.udev.extraRules = ''
